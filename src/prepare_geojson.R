b044c7279fb28d450908b76cfd354918909ed5e4
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2020-03-23 07:09:03 +0100

    update readme

--- src/prepare_geojson.R
+++ src/prepare_geojson.R
@@ -6,3 +6,9 @@ plz <- rgdal::readOGR(paste0(path,"post_pl.shp"))
 
 rgdal::writeOGR(plz, paste0(path,'plz.geojson'),'dataMap', driver='GeoJSON')
 
+
+# Links -------------------------------------------------------------------
+
+# https://dillonshook.com/leaflet-zip-code-map-part-1/
+# https://www.reddit.com/r/javascript/comments/88tkg3/question_about_using_leaflet_with_r/
+# https://leafletjs.com/examples/geojson/
